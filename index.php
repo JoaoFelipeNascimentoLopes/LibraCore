06ceb4cc2b74e730db5ccd2c3cb9c89613f5bc17
JoaoFelipeNascimentoLopes <joaofelipe.nl05@gmail.com>
2024-12-04 14:26:55 -0300

    update index.php (update -page content)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include("php/navbar.php");
+    //include("php/navbar.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -8,8 +8,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="img/icon_Livros.png">
     <title>Home - LibraCore</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+        h1, h2, h3 {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
 </head>
 <body>
-    
+    <br><br>
+    <div style="display: flex; justify-content: center;">
+        <img src="img/icon_LibraCore.png" alt="">
+    </div>
+    <br><br><br>
+    <h1 style="text-align: center;">LibraCore</h1>
+    <h3 style="text-align: center;">Sistema de Gerenciamento de Bibliotecas</h3>
+    <br>
+    <div style="display: flex; justify-content: center; ">
+        <div style="display: block; padding: 1%;">
+
+        </div>
+        <div style="display: block; padding: 1%;">
+            
+        </div>
+    </div>
 </body>
 </html>
