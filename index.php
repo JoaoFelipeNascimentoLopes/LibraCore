41d94fc5b29924ab12903de6dc28e81ee2e17d96
JoaoFelipeNascimentoLopes <joaofelipe.nl05@gmail.com>
2024-12-04 13:46:51 -0300

    update index.php (update -head)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="img/icon_Livros.png">
     <title>Home - LibraCore</title>
 </head>
 <body>
